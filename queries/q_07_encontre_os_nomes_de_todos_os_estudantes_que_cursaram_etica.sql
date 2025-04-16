e6df97d885bee8e8c2ebb114149fdcdab00bd2aa
Lucas Tonoli Cabral Duarte <146139708+Cabral100@users.noreply.github.com>
2025-04-16 11:27:12 -0300

    Update q_07_encontre_os_nomes_de_todos_os_estudantes_que_cursaram_etica.sql

--- queries/q_07_encontre_os_nomes_de_todos_os_estudantes_que_cursaram_etica.sql
+++ queries/q_07_encontre_os_nomes_de_todos_os_estudantes_que_cursaram_etica.sql
@@ -1,14 +1,5 @@
-[?25l
-    Select a project:                                                                             
-                                                                                                  
-  >  1. nlweecqavpfxgrodjtbn [name: Projeto1, org: dbcobxdraadehuwebukk, region: us-west-1]       
-    2. sqjsbawfrwedqattwyxq [name: Banco_de_Dados_1, org: dbcobxdraadehuwebukk, region: us-east-1]
-                                                                                                  
-                                                                                                  
-    ↑/k up • ↓/j down • / filter • q quit • ? more                                                
-                                                                                                  [0D[2K[1A[2K[1A[2K[1A[2K[1A[2K[1A[2K[1A[2K[1A[2K[1A[0D[2K [0D[2K[?25h[?1002l[?1003l[?1006lSELECT DISTINCT a.nome
+SELECT DISTINCT a.nome
 FROM alunos a
 JOIN historico_escolar h ON a.id_aluno = h.id_aluno
 JOIN disciplinas d ON h.id_disciplina = d.id_disciplina
 WHERE d.nome = 'Ética';
-
