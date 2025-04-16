ade34d6ef0ff8d8b382873a2efc201a40355d299
Lucas Tonoli Cabral Duarte <146139708+Cabral100@users.noreply.github.com>
2025-04-16 11:21:37 -0300

    Update ddl.sql

--- queries/ddl.sql
+++ queries/ddl.sql
@@ -1,12 +1,4 @@
-[?25l
-    Select a project:                                                                             
-                                                                                                  
-  >  1. nlweecqavpfxgrodjtbn [name: Projeto1, org: dbcobxdraadehuwebukk, region: us-west-1]       
-    2. sqjsbawfrwedqattwyxq [name: Banco_de_Dados_1, org: dbcobxdraadehuwebukk, region: us-east-1]
-                                                                                                  
-                                                                                                  
-    ↑/k up • ↓/j down • / filter • q quit • ? more                                                
-                                                                                                  [0D[2K[1A[2K[1A[2K[1A[2K[1A[2K[1A[2K[1A[2K[1A[2K[1A[0D[2K [0D[2K[?25h[?1002l[?1003l[?1006lDROP TABLE if exists Departamentos cascade;
+DROP TABLE if exists Departamentos cascade;
 DROP TABLE if exists Cursos cascade;
 DROP TABLE if exists Alunos cascade;
 DROP TABLE if exists Cursos_alunos cascade;
@@ -115,5 +107,4 @@ CREATE TABLE TCCs_Alunos (
     FOREIGN KEY (id_tcc) REFERENCES TCCs(id_tcc) ON DELETE CASCADE,
     FOREIGN KEY (id_aluno) REFERENCES Alunos(id_aluno) ON DELETE CASCADE
 );
-
 
