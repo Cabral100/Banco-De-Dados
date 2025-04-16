b8d5512bb43c64616a6acff35d201a725c66642b
Lucas Tonoli Cabral Duarte <146139708+Cabral100@users.noreply.github.com>
2025-04-16 11:27:57 -0300

    Update q_10_recupere_os_nomes_e_i_ds_dos_estudantes_que_sao_orientados_por_um_professor_especifico_id_12.sql

--- queries/q_10_recupere_os_nomes_e_i_ds_dos_estudantes_que_sao_orientados_por_um_professor_especifico_id_12.sql
+++ queries/q_10_recupere_os_nomes_e_i_ds_dos_estudantes_que_sao_orientados_por_um_professor_especifico_id_12.sql
@@ -1,14 +1,5 @@
-[?25l
-    Select a project:                                                                             
-                                                                                                  
-  >  1. nlweecqavpfxgrodjtbn [name: Projeto1, org: dbcobxdraadehuwebukk, region: us-west-1]       
-    2. sqjsbawfrwedqattwyxq [name: Banco_de_Dados_1, org: dbcobxdraadehuwebukk, region: us-east-1]
-                                                                                                  
-                                                                                                  
-    ↑/k up • ↓/j down • / filter • q quit • ? more                                                
-                                                                                                  [0D[2K[1A[2K[1A[2K[1A[2K[1A[2K[1A[2K[1A[2K[1A[2K[1A[0D[2K [0D[2K[?25h[?1002l[?1003l[?1006lSELECT a.id_aluno, a.nome
+SELECT a.id_aluno, a.nome
 FROM alunos a
 JOIN tccs_alunos ta ON a.id_aluno = ta.id_aluno
 JOIN tccs t ON ta.id_tcc = t.id_tcc
 WHERE t.id_professor_orientador = '12';
-
